331b22a3561ffb6fe7c8f9bfadf416144c0267e5
nishanthmosam <nishanth.mosam@docyt.com>
2025-07-04 18:18:55 +0530

    Handling the output when the input is '1'

---
+++ string_calculator_spec.rb
@@ -0,0 +1,15 @@
+require_relative 'string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      calculator = StringCalculator.new
+      expect(calculator.add("")).to eq(0)
+    end
+
+    it 'returns the number itself for a single number' do
+      calculator = StringCalculator.new
+      expect(calculator.add("1")).to eq(1)
+    end
+  end
+end 
