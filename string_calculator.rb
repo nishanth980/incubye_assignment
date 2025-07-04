6864dad4161ff2d35c5c924af4d91ab902b559a5
nishanthmosam <nishanth.mosam@docyt.com>
2025-07-04 18:36:56 +0530

    Handling newlines between numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,8 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
     
+    # Replace newlines with commas, then split on commas
+    numbers = numbers.gsub("\n", ",")
     numbers.split(',').map(&:to_i).sum
   end
 end 
